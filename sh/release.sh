03eada499a3f42d1977bb7a7b95dd8a04ac2bbef
angel <angelrinascita@gmail.com>
2021-03-08 11:01:56 +0400

    fix

--- sh/release.sh
+++ sh/release.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/bash
 
 if [ ! $1 ]; then
-    echo "Error: you should pass version number"
+    echo "[@a-a-game-studio/aa-front]: creating version"
+    npm version patch
+
+    echo "[@a-a-game-studio/aa-front]: pushing updates"
+    git push
+
+    echo "[@a-a-game-studio/aa-front]: publish updates"
+    npm publish --access public --registry https://registry.npmjs.org
 else
     echo "[@a-a-game-studio/aa-front]: creating version"
     npm version $1

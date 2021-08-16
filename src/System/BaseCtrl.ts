33e208963403d74cd923884e849819582773a856
angel <sakharovsa@63pokupki.ru>
2021-08-16 10:19:00 +0400

    убранны warn при старте

--- src/System/BaseCtrl.ts
+++ src/System/BaseCtrl.ts
@@ -33,34 +33,5 @@ export class BaseCtrl{
     public constructor(vuexSys:VuexSys){
         this.vuexSys = vuexSys;
         this.store = vuexSys.getRootStore();
-
-        if(!this.ix){
-            console.warn('Не определен модуль ix');
-        }
-
-        if(!this.cmd){
-            console.warn('Не определен модуль cmd');
-        }
-
-        if(!this.one){
-            console.warn('Не определен модуль one');
-        }
-
-        if(!this.list){
-            console.warn('Не определен модуль list');
-        }
-
-        if(!this.status){
-            console.warn('Не определен модуль status');
-        }
-
-        if(!this.tree){
-            console.warn('Не определен модуль tree');
-        }
-
-        if(!this.error){
-            console.warn('Не определен модуль error');
-        }
-        
     }
 }
